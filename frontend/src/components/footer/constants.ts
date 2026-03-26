d7206cec4227716a0a3a8efc2a9b4c4c94734cdb
saeed-afr <156814585+saeed-afr@users.noreply.github.com>
2026-03-26 20:47:19 +0300

    did types.ts

---
+++ frontend/src/components/footer/constants.ts
@@ -0,0 +1,24 @@
+import { footerTypes } from "./types";
+
+export FOOTER_DATA: footerNavigation = [
+    {
+        titleKey: "footer.first.title",
+        links: [
+            { label: "footer.first.one", href: "/first" },
+            { label: "footer.first.two", href: "/second" },
+            { label: "footer.first.three", href: "/third" }
+        ],
+        titleKey: "footer.second.title",
+        links: [
+            { label: "footer.first.one", href: "/first" },
+            { label: "footer.first.two", href: "/second" },
+            { label: "footer.first.three", href: "/third" }
+        ],
+        titleKey: "footer.third.title",
+        links: [
+            { label: "footer.first.one", href: "/first" },
+            { label: "footer.first.two", href: "/second" },
+            { label: "footer.first.three", href: "/third" }
+        ]
+    }
+]
---
+++ frontend/src/components/footer/types.ts
@@ -0,0 +1,16 @@
+
+export interface footerSection {
+
+    title: string; //title of the each column
+    links: footerLinks[];
+
+}
+
+export interface footerLinks {
+
+    label: string; //label of the links under each title 
+    href: string;  //link of the each label 
+
+}
+
+export type footerTypes = footerSection[];
