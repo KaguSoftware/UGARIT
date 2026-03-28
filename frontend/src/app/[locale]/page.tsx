cc8895276d2a036a01d4d40dcb47289f1e340aa2
Majed Ahdab <majedahdab@Majeds-MacBook-Air.local>
2026-03-28 20:46:10 +0300

    making it work

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,3 +1,4 @@
+import CategoryCard from "@/src/components/cards/CategoryCard/categoryCard";
 import LocationCard from "@/src/components/cards/LocationCard/LocationCard";
 import { LOCATIONS } from "@/src/components/cards/LocationCard/constants";
 import { Locationtype } from "@/src/components/cards/LocationCard/types";
