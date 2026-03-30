ae441cf9ab159328ef15f555eb3b87c861030b43
Majed Ahdab <majedahdab@Majeds-MacBook-Air.local>
2026-03-30 16:53:12 +0300

    fixed button size changing of show filters

--- frontend/src/components/ui/filters/filters.tsx
+++ frontend/src/components/ui/filters/filters.tsx
@@ -7,16 +7,13 @@ import { Sp } from "./specialProducts/specialProducts";
 
 export const Filters = () => {
     const [isOpen, setIsOpen] = useState(false);
-
     useEffect(() => {
         const mediaQuery = window.matchMedia("(min-width: 768px)");
-
         const handleScreenSizeChange = (
             event: MediaQueryListEvent | MediaQueryList
         ) => {
             setIsOpen(event.matches);
         };
-
         handleScreenSizeChange(mediaQuery);
 
         const listener = (event: MediaQueryListEvent) =>
@@ -29,12 +26,12 @@ export const Filters = () => {
     }, []);
 
     return (
-        <div className="flex flex-col gap-4 p-6">
+        <div className="flex flex-col gap-4 p-6 ">
             <div className="flex items-start justify-between gap-4">
                 <button
                     type="button"
                     onClick={() => setIsOpen((prev) => !prev)}
-                    className="w-fit rounded-md border bg-white p-4 py-2"
+                    className="w-35 rounded-md border bg-white p-4 py-2"
                 >
                     {isOpen ? "Hide Filters" : "Show Filters"}
                 </button>
